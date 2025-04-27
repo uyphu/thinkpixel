ab74774b597df5b828534be745b98a9ea8fce4a5
Phu Le <phu.le@meditpartner.com>
2025-04-27 05:43:00 -0700

    [TP-10] Create About/Help Page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Visualizer from "./pages/Visualizer";
+import About from "./pages/About";
 
 function App() {
   return (
@@ -8,6 +9,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/visualizer/:algorithmName" element={<Visualizer />} />
+        <Route path="/about" element={<About />} /> 
+        {/* Add this route */}
       </Routes>
     </Router>
   );
